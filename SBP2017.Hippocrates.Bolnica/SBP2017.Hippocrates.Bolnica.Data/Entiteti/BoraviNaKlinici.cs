03e7ca4b5e1b663b3b5c071cad55ede8dd090b45
Pavle Cvetkovic <pavlec@elfak.rs>
2017-04-23 01:46:42 +0200

    Djela entiteti

--- SBP2017.Hippocrates.Bolnica/SBP2017.Hippocrates.Bolnica.Data/Entiteti/BoraviNaKlinici.cs
+++ SBP2017.Hippocrates.Bolnica/SBP2017.Hippocrates.Bolnica.Data/Entiteti/BoraviNaKlinici.cs
@@ -9,7 +9,7 @@ namespace SBP2017.Hippocrates.Bolnica.Data.Entiteti
     public class BoraviNaKlinici
     {
         public virtual int Id { get; protected set; }
-        public virtual Klinika KlinikaPacijenta { get; set; }
+        public virtual Klinika KlinikePacijenta { get; set; }
         public virtual PacijentKlinickogCentra Pacijent { get; set; }
         public virtual DateTime DatumPrijema { get; set; }
         public virtual int OcekivaniBoravak { get; set; }
--- SBP2017.Hippocrates.Bolnica/SBP2017.Hippocrates.Bolnica.Data/Entiteti/Rodjak.cs
+++ SBP2017.Hippocrates.Bolnica/SBP2017.Hippocrates.Bolnica.Data/Entiteti/Rodjak.cs
@@ -1,23 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Authentication.ExtendedProtection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SBP2017.Hippocrates.Bolnica.Data.Entiteti
 {
-    public class PacijentKlinickogCentra
+    public class Rodjak
     {
         public virtual int Id { get; protected set; }
         public virtual string Ime { get; set; }
         public virtual string Prezime { get; set; }
-        public virtual DateTime DatumRodjenja { get; set; }
-        public virtual string JMBG { get; set; }
-        public virtual string BracniStatus { get; set; }
-        public virtual string Pol { get; set; }
+        public virtual string Srodstvo { get; set; }
         public virtual string Adresa { get; set; }
-        public virtual Rodjak RodjakPacijenta { get; set; }
+        public virtual string Telefon { get; set; }
+        public virtual IList<PacijentKlinickogCentra> PacijentiUSrodstvu { get; set; }
 
+        public Rodjak()
+        {
+            PacijentiUSrodstvu = new List<PacijentKlinickogCentra>();
+        }
     }
 }
