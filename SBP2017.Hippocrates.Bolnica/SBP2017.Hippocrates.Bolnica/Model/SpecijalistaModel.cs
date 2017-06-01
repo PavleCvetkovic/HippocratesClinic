66d580da373030e1976c2a016ef39727d3977005
Djela <ika.djelic@gmail.com>
2017-06-01 04:15:53 +0200

    Specijalista - model
    To be continued...

--- SBP2017.Hippocrates.Bolnica/SBP2017.Hippocrates.Bolnica/Model/SpecijalistaModel.cs
+++ SBP2017.Hippocrates.Bolnica/SBP2017.Hippocrates.Bolnica/Model/SpecijalistaModel.cs
@@ -15,7 +15,8 @@ namespace SBP2017.Hippocrates.Bolnica.Model
     {
         private Specijalista user; //sakriva zaposlenog iz osnovne klase specijalistom
         private DataTable doctorExams;
-        public DateTime datumPretragePregleda;
+        private List<int> doctorAvailableTimes;        
+        private DateTime datumPretragePregleda; //uvek ce da predstavlja trenutno izabrani datum
 
         public DateTime DatumPretragePregleda {
             get { return datumPretragePregleda; } set { this.datumPretragePregleda = value; } }       
@@ -29,7 +30,10 @@ namespace SBP2017.Hippocrates.Bolnica.Model
             doctorExams.Columns.Add("Datum");
             doctorExams.Columns.Add("Vreme");
             doctorExams.Columns.Add("Prostorija");
-        }
+            //Slobodni termini za zakazivanje
+            doctorAvailableTimes = new List<int>();
+            
+        }    
 
         public override void refreshData()
         {
@@ -39,14 +43,73 @@ namespace SBP2017.Hippocrates.Bolnica.Model
             ISession s = DataLayer.GetSession();
             s.Refresh(user);
 
-            /*//doctorExams
+            //doctorExams
             IQueryOver<Pregled> query =
                 QueryOver.Of<Pregled>().Where(c => c.Specijalista.Id == user.Id && c.Datum == datumPretragePregleda);
-            foreach (Pregled pregled in query)
+            foreach (Pregled pregled in query.List<Pregled>())
             {
                 doctorExams.Rows.Add(pregled.Id, pregled.Datum, pregled.Vreme, pregled.Prostorija);
-            }*/
+            }
+            //doctorAvailableTimes
+            availableTimes();
 
+        }
+
+        public Smena findShiftForDate()
+        {
+            
+            ISession s = DataLayer.GetSession();
+            IQueryOver<Smena> query = QueryOver.Of<Smena>().Where(c=>c.Zaposleni == user);
+            foreach (Smena smena in query.List<Smena>())
+            {
+                if (smena.DatumOd <= datumPretragePregleda && smena.DatumDo >= datumPretragePregleda)
+                {                   
+                    s.Close();
+                    return smena;
+                }
+            }
+            s.Close();
+            return null;
+        }
+
+        public void availableTimes()
+        {
+            Smena smena = findShiftForDate();
+            if (smena != null) //ako je null onda za izabrani datum nije definisana smena
+            {
+                if (smena.TipSmene.Equals("PRVA"))
+                {
+                    for (int i = 700; i <= 1315; i += 45) { doctorAvailableTimes.Add(i); }
+                }
+                else
+                {
+                    for (int i = 1400; i <= 1815; i += 45) { doctorAvailableTimes.Add(i); }
+                }
+                foreach (DataRow pregled in doctorExams.Rows)
+                {
+                    doctorAvailableTimes.Remove((int)pregled["Vreme"]);
+                }
+            }
+        }
+
+
+
+        public void scheduleNewExam(PacijentKlinickogCentra patient, int time) //int je jer se u bazi tako upisuje
+        {
+            ISession s = DataLayer.GetSession();
+
+            Pregled p = new Pregled();
+            p.Specijalista = user;
+            p.Pacijent = patient;
+            p.Datum = datumPretragePregleda;
+            p.Vreme = time;
+            p.Prostorija = user.BrojOrdinacije; //ova verzija f-je uzima default ordinaciju specijaliste
+
+            s.Save(p);
+            s.Flush();
+            s.Close();
+
+            refreshData();
         }
     }
 }
