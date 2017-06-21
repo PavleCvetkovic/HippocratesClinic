885b1b0d5265ab1558adf057e873861599e3e291
Nikola <ceranic@elfak.rs>
2017-06-22 00:13:08 +0200

    CentralniMagacinController - Implementiran

---
+++ SBP2017.Hippocrates.Bolnica/SBP2017.Hippocrates.Bolnica.Data/Data Transfer Objects/CentralniMagacinDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SBP2017.Hippocrates.Bolnica.Data.Data_Transfer_Objects
+{
+    public class CentralniMagacinDto
+    {
+        public virtual int Id { get; set; }
+        public virtual int IdKlinickiCentar { get; set; }
+    }
+}
