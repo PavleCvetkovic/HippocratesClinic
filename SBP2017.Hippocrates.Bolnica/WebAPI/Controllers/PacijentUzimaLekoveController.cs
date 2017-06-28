54368e70fea5791e83cf0f733d9bb8c6e71c67e6
Nikola <ceranic@elfak.rs>
2017-06-28 14:55:07 +0200

    PacijentUzimaLekoveController - Implementiran

---
+++ SBP2017.Hippocrates.Bolnica/WebAPI/Controllers/PacijentUzimaLekoveController.cs
@@ -0,0 +1,61 @@
+﻿using SBP2017.Hippocrates.Bolnica.Data.Data_Transfer_Objects;
+using SBP2017.Hippocrates.Bolnica.Data.DataProvider;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPI.Controllers
+{
+    public class PacijentUzimaLekoveController : ApiController
+    {
+        public IHttpActionResult GetAll()
+        {
+            IList<PacijentUzimaLekoveDto> listDto = PacijentUzimaLekoveDataProvider.GetAll();
+            if (listDto.Count == 0)
+                return Content(HttpStatusCode.NotFound, "Nema rezultata");
+            return Content(HttpStatusCode.OK, listDto);
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+
+            bool found = true;
+            PacijentUzimaLekoveDto d = PacijentUzimaLekoveDataProvider.Get(id, out found);
+            if (!found)
+                return Content(HttpStatusCode.NotFound, "Nema rezultata za trazeni ID: " + id);
+            return Content(HttpStatusCode.OK, d);
+        }
+
+
+        public IHttpActionResult Post([FromBody]PacijentUzimaLekoveDto value)
+        {
+            string s = "";
+            if (PacijentUzimaLekoveDataProvider.Add(value, out s))
+                return Content(HttpStatusCode.Created, s);
+            return Content(HttpStatusCode.BadRequest, s);
+
+        }
+
+        public IHttpActionResult Put(int id, [FromBody]PacijentUzimaLekoveDto value)
+        {
+            string st = "";
+            if (PacijentUzimaLekoveDataProvider.Update(id, value, out st))
+                return Content(HttpStatusCode.OK, st);
+            else
+                return Content(HttpStatusCode.BadRequest, st);
+        }
+
+
+        public IHttpActionResult Delete(int id)
+        {
+            string st = "";
+            if (PacijentUzimaLekoveDataProvider.Delete(id, out st))
+                return Content(HttpStatusCode.OK, st);
+            else
+                return Content(HttpStatusCode.BadRequest, st);
+        }
+    }
+}
