122d2b7d3fc5dbd2be10f42b993c4ee4420647c5
Nikola <ceranic@elfak.rs>
2017-06-28 14:40:23 +0200

    PacijentiCekajuController - Implementiran

---
+++ SBP2017.Hippocrates.Bolnica/SBP2017.Hippocrates.Bolnica.Data/DataProvider/PacijentiCekajuDataProvider.cs
@@ -0,0 +1,139 @@
+﻿using NHibernate;
+using SBP2017.Hippocrates.Bolnica.Data.Data_Transfer_Objects;
+using SBP2017.Hippocrates.Bolnica.Data.Entiteti;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SBP2017.Hippocrates.Bolnica.Data.DataProvider
+{
+    public class PacijentiCekajuDataProvider
+    {
+        public static IList<PacijentiCekajuDto> GetAll()
+        {
+            //st = "";
+            ISession s = DataLayer.GetSession();
+            IList<PacijentiCekaju> iskustva = s.QueryOver<PacijentiCekaju>().List();
+            List<PacijentiCekajuDto> listDTO = new List<PacijentiCekajuDto>();
+            foreach (PacijentiCekaju i in iskustva)
+            {
+                PacijentiCekajuDto dto = new PacijentiCekajuDto()
+                {
+                    Id = i.Id,
+                    DatumUpisa = i.DatumUpisa,
+                    OcekivanoVremeCekanja = i.OcekivanoVremeCekanja,
+                    IdPacijent = i.Pacijent == null ? -1 : i.Pacijent.Id,
+                    IdListaCekanja = i.ListaCekanja == null ? -1 : i.ListaCekanja.Id
+                };
+                listDTO.Add(dto);
+            }
+            return listDTO;
+        }
+
+        public static PacijentiCekajuDto Get(int id, out bool found)
+        {
+            found = true;
+            ISession s = DataLayer.GetSession();
+            PacijentiCekaju i = s.Get<PacijentiCekaju>(id);
+            if (i == null)
+            {
+                s.Close();
+                found = false;
+                return new PacijentiCekajuDto();
+            }
+            PacijentiCekajuDto kdto = new PacijentiCekajuDto()
+            {
+                Id = i.Id,
+                DatumUpisa = i.DatumUpisa,
+                OcekivanoVremeCekanja = i.OcekivanoVremeCekanja,
+                IdPacijent = i.Pacijent == null ? -1 : i.Pacijent.Id,
+                IdListaCekanja = i.ListaCekanja == null ? -1 : i.ListaCekanja.Id
+            };
+            s.Close();
+            return kdto;
+        }
+
+        public static bool Add(PacijentiCekajuDto i, out string success)
+        {
+            ISession s = DataLayer.GetSession();
+            success = "Uspesno upisan objekat";
+            PacijentiCekaju d = new PacijentiCekaju();
+
+
+            //d.Id = i.Id;
+            d.DatumUpisa = i.DatumUpisa;
+            d.OcekivanoVremeCekanja = i.OcekivanoVremeCekanja;
+            d.Pacijent = s.Get< PacijentKlinickogCentra> (i.IdPacijent);
+            d.ListaCekanja = s.Get<ListaCekanja>(i.IdListaCekanja);
+            try
+            {
+                s.Save(d);
+                s.Flush();
+                s.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                s.Close();
+                success = "Greska prilikom upisa u bazu " + ex.Message;
+                return false;
+            }
+        }
+
+        public static bool Update(int id, PacijentiCekajuDto i, out string success)
+        {
+            ISession s = DataLayer.GetSession();
+            success = "Uspesno upisan/azuiran objekat";
+
+            PacijentiCekaju obj = s.Get<PacijentiCekaju>(id);
+            if (obj == null)
+                obj = new PacijentiCekaju();
+
+            obj.DatumUpisa = i.DatumUpisa;
+            obj.OcekivanoVremeCekanja = i.OcekivanoVremeCekanja;
+            obj.Pacijent = s.Get<PacijentKlinickogCentra>(i.IdPacijent);
+            obj.ListaCekanja = s.Get<ListaCekanja>(i.IdListaCekanja);
+            try
+            {
+                s.Save(obj);
+                s.Flush();
+                s.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                s.Close();
+                success = "Greska prilikom upisa u bazu " + ex.Message;
+                return false;
+            }
+        }
+
+        public static bool Delete(int id, out string success)
+        {
+            ISession s = DataLayer.GetSession();
+            success = "Uspesno obrisan objekat";
+
+            PacijentiCekaju d = s.Get<PacijentiCekaju>(id);
+            if (d == null)
+            {
+                success = "Ne postoji objekata sa ID: " + id;
+                return false;
+            }
+            try
+            {
+                s.Delete(d);
+                s.Flush();
+                s.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                s.Close();
+                success = "Greska prilikom upisa u bazu " + ex.Message;
+                return false;
+            }
+        }
+    }
+}
---
+++ SBP2017.Hippocrates.Bolnica/WebAPI/Controllers/PacijentiCekajuController.cs
@@ -0,0 +1,61 @@
+﻿using SBP2017.Hippocrates.Bolnica.Data.Data_Transfer_Objects;
+using SBP2017.Hippocrates.Bolnica.Data.DataProvider;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPI.Controllers
+{
+    public class PacijentiCekajuController : ApiController
+    {
+        public IHttpActionResult GetAll()
+        {
+            IList<PacijentiCekajuDto> listDto = PacijentiCekajuDataProvider.GetAll();
+            if (listDto.Count == 0)
+                return Content(HttpStatusCode.NotFound, "Nema rezultata");
+            return Content(HttpStatusCode.OK, listDto);
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+
+            bool found = true;
+            PacijentiCekajuDto d = PacijentiCekajuDataProvider.Get(id, out found);
+            if (!found)
+                return Content(HttpStatusCode.NotFound, "Nema rezultata za trazeni ID: " + id);
+            return Content(HttpStatusCode.OK, d);
+        }
+
+
+        public IHttpActionResult Post([FromBody]PacijentiCekajuDto value)
+        {
+            string s = "";
+            if (PacijentiCekajuDataProvider.Add(value, out s))
+                return Content(HttpStatusCode.Created, s);
+            return Content(HttpStatusCode.BadRequest, s);
+
+        }
+
+        public IHttpActionResult Put(int id, [FromBody]PacijentiCekajuDto value)
+        {
+            string st = "";
+            if (PacijentiCekajuDataProvider.Update(id, value, out st))
+                return Content(HttpStatusCode.OK, st);
+            else
+                return Content(HttpStatusCode.BadRequest, st);
+        }
+
+
+        public IHttpActionResult Delete(int id)
+        {
+            string st = "";
+            if (PacijentiCekajuDataProvider.Delete(id, out st))
+                return Content(HttpStatusCode.OK, st);
+            else
+                return Content(HttpStatusCode.BadRequest, st);
+        }
+    }
+}
