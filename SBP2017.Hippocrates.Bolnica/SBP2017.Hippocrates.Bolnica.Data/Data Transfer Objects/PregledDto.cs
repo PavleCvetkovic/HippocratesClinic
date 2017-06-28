6d1e8217005372916271104dcc93170f6cb7de76
Nikola <ceranic@elfak.rs>
2017-06-28 15:16:35 +0200

    PregledController (poslednji) - Implementiran

---
+++ SBP2017.Hippocrates.Bolnica/SBP2017.Hippocrates.Bolnica.Data/Data Transfer Objects/PregledDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SBP2017.Hippocrates.Bolnica.Data.Data_Transfer_Objects
+{
+    public class PregledDto
+    {
+        public virtual int Id { get;  set; }
+        public virtual DateTime Datum { get; set; }
+        public virtual int Vreme { get; set; }
+        public virtual string Prostorija { get; set; }
+        public virtual string IdIzabranogLekara { get; set; } //ide iz druge baze //CHECK // nullable  u bazi
+        public virtual int IdSpecijalista { get; set; }
+        public virtual int IdPacijent { get; set; }
+    }
+}
